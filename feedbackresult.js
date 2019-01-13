7b3163806c81e581860df3a65ba85c83131d0729
Heavenwar <christopherlimawan@gmail.com>
2019-01-13 21:22:59 +0700

    Repair some minor fixes

--- feedbackresult.js
+++ feedbackresult.js
@@ -1,5 +1,5 @@
 let value = feedback;
 
 if(value.includes("<SCRIPT>") && value.includes("</SCRIPT>") && value.length > 17){  
-    alert("TEAM4{xss_cl4ss1c_expl0it_c6fca}");
+    alert("TEAM_4{xss_cl4ss1c_expl0it_c6fca}");
 }
